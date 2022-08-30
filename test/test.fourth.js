19c9bd2c567b978d68e1d43ea1175a60fb22adb3
Sbonga Nzimande <Nzimandesbonga@gmail.com>
2022-08-30 14:27:45 +0200

    second commit

--- test/test.fourth.js
+++
@@ -1,6 +0,0 @@
-describe("The fourth test", function() {
-
-    it("should be able to download saved documents", function() {
-        assert.equal(1, 1);
-    });
-});
--- test/test.third.js
+++
@@ -1,6 +0,0 @@
-describe("The third test", function() {
-
-    it("should be able to show more information", function() {
-        assert.equal(1, 1);
-    });
-});
